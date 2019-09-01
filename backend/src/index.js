341459f95ba26815dcada0749fb50aaed16369ba
Andrii Muntian <work.vesel4ak@gmail.com>
2019-09-01 23:19:47 +0300

    graphql-yoga server set up

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,27 @@
-// let's go!
+// set env variables up
+const path = require('path');
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+
+const dotenvPath = path.resolve(process.cwd(), 'variables.env');
+const myEnv = dotenv.config({ path: dotenvPath });
+dotenvExpand(myEnv);
+
+// create yoga server
+const createServer = require('./createServer');
+// const db = require('./db');
+
+const server = createServer();
+
+// TODO: use express middleware to handle cookies (JWT)
+// TODO: use express middleware to populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, process.env.PLAYGROUND_URL],
+  },
+}, (deets) => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is now running on http://localhost:${deets.port}`);
+});
